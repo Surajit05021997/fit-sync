6698594e4bc4f97d70cf1b9fd8e6f8c3b1cde9d2
Surajit Maity <surajitmaity12345@gmail.com>
2025-05-03 17:52:04 +0530

    added redirection of user from landing page to dashboard page if user already logged in

--- src/app/modules/public/pages/landing/landing.component.ts
+++ src/app/modules/public/pages/landing/landing.component.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from 'src/app/core/services';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
   phoneNumber: number | null = null;
   otpCode: number | null = null;
   otpSent: boolean = false;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private afAuth: AngularFireAuth
+  ) {}
+
+  ngOnInit() {
+    // Check if user is logged in or not. If logged in then redirect user to dashboard page.
+    const obs = this.afAuth.authState.pipe(map((user) => !!user));
+    obs.subscribe((isUserLoggedIn) => {
+      if (isUserLoggedIn) {
+        this.router.navigate(['/dashboard']);
+      }
+    });
+  }
 
   // Google Sign-In
   async signInWithGoogle() {
